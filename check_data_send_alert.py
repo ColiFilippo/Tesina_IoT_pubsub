00b9d8aef2f9041886ed2eaf64d787c0494937da
Coli Filippo <tesina135@gmail.com>
2023-11-05 17:37:44 +0100

    First Commit

---
+++ check_data_send_alert.py
@@ -0,0 +1,111 @@
+import json
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+from google.auth import jwt
+from google.cloud import firestore, pubsub_v1
+
+from secret import smtp_username, smtp_password, topic_name, project_id
+
+'''IMPORTANTE: Questo programma è da lanciare prima di pub_data_from_xlxs'''
+
+# Configura publisher e subscriber
+service_account_info = json.load(open("credentials.json"))
+audience = "https://pubsub.googleapis.com/google.pubsub.v1.Subscriber"
+credentials = jwt.Credentials.from_service_account_info(
+    service_account_info, audience=audience
+)
+subscriber = pubsub_v1.SubscriberClient(credentials=credentials)
+
+service_account_info = json.load(open("credentials.json"))
+audience = "https://pubsub.googleapis.com/google.pubsub.v1.Publisher"
+credentials = jwt.Credentials.from_service_account_info(
+    service_account_info, audience=audience
+)
+publisher = pubsub_v1.PublisherClient(credentials=credentials)
+topic_path = subscriber.topic_path(project_id, topic_name)
+try:
+    topic = publisher.create_topic(request={"name": topic_path})
+    print(f"Created topic: {topic.name}")
+except Exception as e:
+    print(e)
+
+# Configura il client Firestore
+db = firestore.Client.from_service_account_json('credentials.json')
+
+# Configura le informazioni del server SMTP per inviare email
+smtp_server = 'smtp.gmail.com'
+#smtp_port = 465
+smtp_port = 587
+
+# Crea un elenco per tenere traccia dei messaggi già processati
+processed_messages = set()
+
+def send_email(subject, message, recipient):
+    # Configura il messaggio email
+    msg = MIMEMultipart()
+    msg['From'] = smtp_username
+    msg['To'] = recipient
+    msg['Subject'] = subject
+    msg.attach(MIMEText(message, 'plain'))
+
+    # Inizializza il client SMTP
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.starttls()
+    server.login(smtp_username, smtp_password)
+    print("Connessione SMTP iniziata...")
+    # Invia l'email
+    server.sendmail(smtp_username, recipient, msg.as_string())
+    print(f"Mail Inviata a {recipient}")
+    # Chiudi la connessione SMTP
+    server.quit()
+    print("Connessione SMTP chiusa...")
+
+def process_sensor_data(data):
+
+    # Recupera tutti gli utenti dal database
+    users = db.collection('users').stream()
+
+    for user in users:
+        user_data = user.to_dict()
+        threshold_value = user_data['threshold_value']
+        sensor_type = user_data['sensor_type']
+
+        if data.get(sensor_type) is not None and data[sensor_type] > threshold_value:
+            subject = f"Soglia superata per {sensor_type}"
+            message = f"Il valore di {sensor_type} ha superato la soglia di {threshold_value}."
+            send_email(subject, message, user.id)
+            break
+
+if __name__ == '__main__':
+
+    # Configura il client Pub/Sub
+    subscription_name = 'pubsub1'
+    subscription_path = subscriber.subscription_path(project_id, subscription_name)
+    print(subscription_path)
+    # create subscription
+    try:
+        subscriber.create_subscription(name=subscription_path, topic=topic_path)
+    except Exception as e:
+        print(e)
+
+    def callback(message):
+        #Decodifica il messaggio
+        data_json = message.data.decode('utf-8')
+        data = json.loads(data_json)
+        print(data_json)
+        process_sensor_data(data)
+        message.ack()
+
+
+
+
+    streaming_pull = subscriber.subscribe(subscription_path, callback=callback)
+    streaming_pull.result()
+        #print('In attesa di dati dal sensore...')
+        # Aggiungi un ritardo prima di continuare a verificare i messaggi
+        #time.sleep(10)  # Verifica i messaggi ogni 10 secondi, ad esempio
+
+
+
---
+++ pub_data_from_xlsx.py
@@ -0,0 +1,58 @@
+import time
+import openpyxl
+from google.cloud import pubsub_v1
+from datetime import datetime
+from secret import topic_name, project_id
+from google.auth import jwt
+import json
+
+'''IMPORTANTE: Questo programma è da lanciare DOPO aver lanciato check_data_send_alert'''
+
+# Creare il publisher
+service_account_info = json.load(open("credentials.json"))
+audience = "https://pubsub.googleapis.com/google.pubsub.v1.Publisher"
+credentials = jwt.Credentials.from_service_account_info(service_account_info, audience=audience)
+publisher = pubsub_v1.PublisherClient(credentials=credentials)
+
+topic_path = publisher.topic_path(project_id, topic_name)
+print(topic_path)
+try:
+    topic = publisher.create_topic(request={"name": topic_path})
+    print(f"Created topic: {topic.name}")
+except Exception as e:
+    print(e)
+
+# Funzione per leggere le righe da un file Excel e inviarle a Pub/Sub
+def publish_excel_data(file_path):
+    workbook = openpyxl.load_workbook(file_path)
+    sheet = workbook.active
+
+    for row in sheet.iter_rows(values_only=True):
+        if len(row) == 6:  # Assicurati che ci siano 6 colonne nei dati
+            # Converte la data nel formato "yyyy-mm-gg"
+            date_str = str(row[0])
+            date_obj = datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S")
+            formatted_date = date_obj.strftime("%Y-%m-%d")
+
+            data_dict = {
+                "Date": formatted_date,
+                "Temperatura massima": float(row[1]),
+                "Temperatura minima": float(row[2]),
+                "Velocita del vento": float(row[3]),
+                "Umidita": float(row[4]),
+                "Precipitazioni": float(row[5])
+            }
+            #converti in json per risolvere problema dizionario non inviabile con pubsub
+            data = json.dumps(data_dict)
+
+        # Invia i dati al topic Pub/Sub
+        publisher.publish(topic_path, data.encode('utf-8'))
+        print(f'Dati inviati: {data}')
+
+        # Intervalli di invio (ad esempio, 1 secondo)
+        time.sleep(10)
+
+
+if __name__ == '__main__':
+    excel_file_path = 'Farm_Weather_Data.xlsx'
+    publish_excel_data(excel_file_path)
